b00ecd0f6cb0b562922f3af60296e157608ee724
Jordyn Espenant <jordynespenant@outlook.com>
2021-11-04 14:20:53 -0600

    Initial commit

---
+++ src/main/java/com/example/a3/DrawingApp.java
@@ -0,0 +1,23 @@
+package com.example.a3;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class DrawingApp extends Application {
+    @Override
+    public void start(Stage stage) {
+
+        StackPane temp = new StackPane();
+
+        Scene scene = new Scene(temp, 320, 240);
+        stage.setTitle("Drawing Application!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
